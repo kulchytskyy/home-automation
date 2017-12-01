2d4f8d8cd7bac8d942ea51d222db8cabc73ea4c7
Andriy Kulchytskyy <akulchytskyy@n-ix.com>
2017-12-01 10:03:32 +0200

    renamed and restructured

--- send.sh
+++ send.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 
-W1_DEVICES_DIR="/sys/bus/w1/devices"
 API_KEY="BQ3BEPLX2OPFEC81"
 API_KEY2="47AQH7TEOTQPFNJY"
 API_URL="https://api.thingspeak.com/update.json"
-
-TEMP1=$(./get_temp.sh "28-0416812d86ff")
-echo "TEMP1=$TEMP1"
-
-TEMP2=$(./get_temp.sh "28-0416816c14ff")
-echo "TEMP2=$TEMP2"
-
-TEMP3=$(./get_temp.sh "28-0000052d025e")
-echo "TEMP3=$TEMP3"
-
 
 URL_PARAMS=""
 FIELD_NUM=1
@@ -23,54 +12,63 @@ FIELD_NUM2=1
 
 WORK_DIR=$(pwd)
 
-#cd $W1_DEVICES_DIR
-#for DEVICE in *; do
-#  if [[ $DEVICE != w1_bus_master* ]]; then
-#    TEMP=$($WORK_DIR/get_temp.sh "$DEVICE")
-#    echo $DEVICE $FIELD_NUM $TEMP
-#    URL_PARAMS="$URL_PARAMS&field$FIELD_NUM=$TEMP"
-#    FIELD_NUM=$(($FIELD_NUM+1))
-#  fi
-#done
-
+###
+### ea2
+###
 for i in $(seq 1 3); do
-   EA2T=`/usr/local/sbin/ea2temp_get.sh $i`
+   EA2T=`./temperature/ea2/get.sh $i`
    echo "EA2T$i=$EA2T"
    URL_PARAMS="$URL_PARAMS&field$FIELD_NUM=$EA2T"
    FIELD_NUM=$(($FIELD_NUM+1))
 done
 
-`./emodul_get.sh`
 
-EM_TEMP1=`./emodul_parse.sh 1006 sensor`
+###
+###	emodul
+###
+`./emodul/get.sh`
+
+EM_TEMP1=`./emodul/parse.sh 1006 sensor`
 echo "EM_TEMP1=$EM_TEMP1"
 URL_PARAMS="$URL_PARAMS&field$FIELD_NUM=$EM_TEMP1"
 FIELD_NUM=$(($FIELD_NUM+1))
 
-#EM_TEMP2=`./emodul_parse.sh 1008 sensor`
+#EM_TEMP2=`./emodul/parse.sh 1008 sensor`
 #echo "EM_TEMP2=$EM_TEMP2"
 #URL_PARAMS="$URL_PARAMS&field$FIELD_NUM=$EM_TEMP2"
 #FIELD_NUM=$(($FIELD_NUM+1))
 
-EM_TEMP3=`./emodul_parse.sh 1009 sensor`
+EM_TEMP3=`./emodul/parse.sh 1009 sensor`
 echo "EM_TEMP3=$EM_TEMP3"
 URL_PARAMS="$URL_PARAMS&field$FIELD_NUM=$EM_TEMP3"
 FIELD_NUM=$(($FIELD_NUM+1))
 
-EM_FAN=`./emodul_parse.sh 1011 fan`
+EM_FAN=`./emodul/parse.sh 1011 fan`
 echo "EM_FAN=$EM_FAN"
 URL_PARAMS="$URL_PARAMS&field$FIELD_NUM=$EM_FAN"
 FIELD_NUM=$(($FIELD_NUM+1))
 
+###
+### w1
+###
+TEMP1=$(./temperature/w1/get.sh "28-0416812d86ff")
+echo "TEMP1=$TEMP1"
 URL_PARAMS2="$URL_PARAMS2&field$FIELD_NUM2=$TEMP1"
 FIELD_NUM2=$(($FIELD_NUM2+1))
 
+TEMP2=$(./temperature/w1/get.sh "28-0416816c14ff")
+echo "TEMP2=$TEMP2"
 URL_PARAMS2="$URL_PARAMS2&field$FIELD_NUM2=$TEMP2"
 FIELD_NUM2=$(($FIELD_NUM2+1))
 
+TEMP3=$(./temperature/w1/get.sh "28-0000052d025e")
+echo "TEMP3=$TEMP3"
 URL_PARAMS2="$URL_PARAMS2&field$FIELD_NUM2=$TEMP3"
 FIELD_NUM2=$(($FIELD_NUM2+1))
 
+###
+### send
+###
 echo $(date)
 echo "url_params=$URL_PARAMS"
 wget -qO- "$API_URL?api_key=$API_KEY&$URL_PARAMS"
