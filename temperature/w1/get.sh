ec2b09daa20d45dfde5cd2a30fce2f9aabc85920
root <root@Bubuntu.N-IX.LOCAL>
2018-04-25 14:53:30 +0000

    w1 temp validation

--- temperature/w1/get.sh
+++ temperature/w1/get.sh
@@ -9,4 +9,8 @@ TEMP_RAW=$(cat "/sys/bus/w1/devices/$DEVICE_ID/w1_slave")
 TEMP_NUMERIC=$(echo "$TEMP_RAW" | grep t= |  cut -f2 -d= | awk '{print $1/1000}')
 #echo "numeric=$TEMP_NUMERIC"
 
-echo $TEMP_NUMERIC
+if (( $(echo "$TEMP_NUMERIC < 60" | bc -l) )) && (( $(echo "$TEMP_NUMERIC > -30" | bc -l) )); then
+	echo $TEMP_NUMERIC
+else
+	echo ""
+fi
