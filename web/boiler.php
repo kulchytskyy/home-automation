30e5c8fbdf39c330b38448ae76c61e34e132021b
Andriy Kulchytskyy <kulchytskyy@gmail.com>
2018-11-21 09:01:39 +0200

    Added web

---
+++ web/boiler.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+
+<?php
+  $action = $_POST['action'];
+  $hour = $_POST['hour'];
+  $min = $_POST['min'];
+?>
+
+<?php
+	echo "action=$action<br/>";
+	echo "hour=$hour<br/>";
+	echo "min=$min<br/>";
+?>
+
+</body>
+</html>
+
