56af90623864d9b4ec951b37a13451aa9da11522
Andriy Kulchytskyy <kulchytskyy@gmail.com>
2019-01-22 12:05:21 +0200

    Updated load2mysql script

--- sql/load2mysql.sql
+++ sql/load2mysql.sql
@@ -1,6 +1,6 @@
 use t;
 
-CREATE TABLE `t1803` (
+CREATE TABLE `t1901` (
   `date_str` varchar(45) DEFAULT NULL,
   `id_str` varchar(45) DEFAULT NULL,
   `field1_str` varchar(45) DEFAULT NULL,
@@ -20,81 +20,94 @@ CREATE TABLE `t1803` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
-LOAD DATA LOCAL INFILE 'C:/Temp/feed.csv'
-INTO TABLE t.t1803 FIELDS TERMINATED BY ','
+LOAD DATA LOCAL INFILE 'C:/Temp/feeds.csv'
+INTO TABLE t.t1901 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n';
 
-select * from t1803
+select * from t1901
 order by id_str;
 
 SET SQL_SAFE_UPDATES = 0;
 -- delete from t1801;
 
 select CONVERT(`id_str`,UNSIGNED)
-FROM t1803
+FROM t1901
 where CONVERT(`id_str`,UNSIGNED) is null;
 
 
 SELECT (`id_str` * 1)  
-FROM t1803;
+FROM t1901;
 
-update t1803
+update t1901
 set id=CONVERT(`id_str`,UNSIGNED);
 
 SELECT CAST(date_str as datetime), replace(date_str,' UTC','')
-FROM t1803
+FROM t1901
 where CAST(replace(date_str,' UTC','') as datetime) is null;
 
-update t1803
+update t1901
 set date=CAST(replace(date_str,' UTC','') as datetime);
 
 select CAST(`field1_str` as decimal(10,3))
-from t1803
+from t1901
 where field1_str is not null and field1_str <> ''
 order by CAST(`field1_str` as decimal(10,3));
 
-update t1803
+update t1901
 set field1=field1_str
 where field1_str is not null and field1_str <> '' and field1_str <> '""';
 
-update t1803
+update t1901
 set field2=field2_str
 where field2_str is not null and field2_str <> '' and field2_str <> '""';
 
-update t1803
+update t1901
 set field3=field3_str
 where field3_str is not null and field3_str <> '' and field3_str <> '""';
 
-update t1803
+update t1901
 set field4=field4_str
 where field4_str is not null and field4_str <> '' and field4_str <> '""';
 
-update t1803
+update t1901
 set field5=field5_str
 where field5_str is not null and field5_str <> '' and field5_str <> '""';
 
-update t1803
+update t1901
 set field6=field6_str
 where field6_str is not null and field6_str <> '' and field6_str <> '""';
 
-delete from t1803
-where field1_str='""' or field2_str='""' or field3_str='""' or field4_str='""' or field5_str='""' or field6_str='""' ;
+-- select * from t1901
+-- where field1_str='""' or field2_str='""' or field3_str='""' or field4_str='""' or field5_str='""' or field6_str='""' ;
 
-select * from t1803
-where (field1 > 50 or field2 > 50 or field3 > 50 or field5 > 50);
+-- delete from t1901
+-- where field1_str='""' or field2_str='""' or field3_str='""' or field4_str='""' or field5_str='""' or field6_str='""' ;
 
-delete from t1803
-where (field1 < -20 or field2 < -20 or field3 < -20 or field5 < -20);
+update t1901
+set field1=null
+where field1 > 50 or field1 < -20;
 
-select * from t1803
-where (field1 < 0)
-and date > '2017-12-01';
+update t1901
+set field2=null
+where field2 > 50 or field2 < -20;
 
-delete from t1803
-where date < '2017-12-01';
+update t1901
+set field3=null
+where field3 > 50 or field3 < -20;
 
-delete from t1803
-where id in (94655,94656,94657);
+update t1901
+set field5=null
+where field5 > 50 or field5 < -20;
 
-delete from t1803
-where date_str='created_at';
+select * from t1901
+where (field1 < 0);
+-- and date > '2017-12-01';
+
+select * from t1901
+where date < '2017-12-01' and date > '2017-06-01';
+
+delete from t1901
+where date < '2017-06-01';
+
+-- delete from t1901
+-- where date_str='created_at';
--- sql/stats.sql
+++ sql/stats.sql
@@ -1,48 +1,48 @@
-SELECT year(date), month(date), min(field1), max(field1), avg(field1)
-FROM t.t1803
+SELECT year(date), month(date), round(avg(field1), 1), round(min(field1),1), round(max(field1),1)
+FROM t.t1901
 group by year(date), month(date);
 
-SELECT year(date), month(date), min(field5), max(field5), avg(field5)
-FROM t.t1803
+SELECT year(date), month(date), round(avg(field5), 1), round(min(field5),1), round(max(field5),1)
+FROM t.t1901
 group by year(date), month(date);
 
-SELECT year(date), month(date), min(field3), max(field3), avg(field3)
-FROM t.t1803
+SELECT year(date), month(date), round(avg(field3), 1), round(min(field3),1), round(max(field3),1)
+FROM t.t1901
 group by year(date), month(date);
 
 SELECT count(*)/(
 	SELECT count(*)
-	FROM t.t1803)*100
-FROM t.t1803
+	FROM t.t1901)*100
+FROM t.t1901
 where field6>0;
 
 SELECT count(*)/(
 	SELECT count(*)
-	FROM t.t1801)*100
-FROM t.t1801
+	FROM t.t1901)*100
+FROM t.t1901
 where field6<=0;
 
 SELECT count(*)
-FROM t.t1801
+FROM t.t1901
 where field6<=50;
 
 SELECT count(*)
-FROM t.t1801;
+FROM t.t1901;
 
 SELECT sum(field6/100*16000)/5/1000, avg(field5), avg(field1)
-FROM t.t1801
+FROM t.t1901
 where date<'2017-12-10';
 
 SELECT sum(field6/100*16000)/5/1000, avg(field5), avg(field1)
-FROM t.t1801
+FROM t.t1901
 where date>='2017-12-10' and date<'2017-12-20';
 
 SELECT sum(field6/100*16000)/5/1000, avg(field5), avg(field1)
-FROM t.t1801
+FROM t.t1901
 where date>='2017-12-20' and date<'2017-12-30';
 
 SELECT year(date), month(date), round(sum(field6/100*16000)/5/1000), round(avg(field5), 2), round(avg(field1), 2)
-FROM t.t1803
+FROM t.t1901
 group by year(date), month(date);
 
 
@@ -50,8 +50,14 @@ group by year(date), month(date);
 -- min = 0.85 kg/h
 
 SELECT year(date), month(date), round(sum((3.4-0.85)*field6/100+0.85)/60*5*.7,0), round(avg(field5), 2), round(avg(field1), 2)
-FROM t.t1803
+FROM t.t1901
 where field6 > 0
 group by year(date), month(date);
 
+-- sunny days
+SELECT year(date), month(date), day(date), round(count(*)*5/60,1)
+FROM t.t1901
+where field3 > 15 and (month(date)>=11 or month(date)<=3)
+group by year(date), month(date), day(date)
+having count(*)>5;
 
