9c6a024a8011459649473047461f8fbab029b867
Andriy Kulchytskyy <kulchytskyy@gmail.com>
2020-06-22 15:26:32 +0300

    fixed paths

--- upload/pi/channel1.sh
+++ upload/pi/channel1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DIR=$(dirname $0)
-source $DIR/config.sh
+source $DIR/../config.sh
 
 API_KEY=$API_KEY1
 
@@ -14,7 +14,7 @@ echo $(date)
 ### ea2
 ###
 for i in 15 14 35; do
-   EA2T=`$DIR/../temperature/ea2/get_temp.sh $i`
+   EA2T=`$DIR/../../temperature/ea2/get_temp.sh $i`
    echo "EA2T$i=$EA2T"
    if [ ! -z $EA2T ]; then
       URL_PARAMS="$URL_PARAMS&field$FIELD_NUM=$EA2T"
@@ -27,27 +27,27 @@ done
 ###	emodul
 ###
 
-EMODULE_STATUS=`$DIR/../emodul/get.sh`
+EMODULE_STATUS=`$DIR/../../emodul/get.sh`
 echo "EMODULE_STATUS=$EMODULE_STATUS"
 
 if [ "$EMODULE_STATUS" = 'active' ]; then 
 
-	EM_TEMP1=`$DIR/../emodul/parse.sh 1006 sensor`
+	EM_TEMP1=`$DIR/../../emodul/parse.sh 1006 sensor`
 	echo "EM_TEMP1=$EM_TEMP1"
 	URL_PARAMS="$URL_PARAMS&field$FIELD_NUM=$EM_TEMP1"
 	FIELD_NUM=$(($FIELD_NUM+1))
 
-	#EM_TEMP2=`$DIR/../emodul/parse.sh 1008 sensor`
+	#EM_TEMP2=`$DIR/../../emodul/parse.sh 1008 sensor`
 	#echo "EM_TEMP2=$EM_TEMP2"
 	#URL_PARAMS="$URL_PARAMS&field$FIELD_NUM=$EM_TEMP2"
 	#FIELD_NUM=$(($FIELD_NUM+1))
 
-	EM_TEMP3=`$DIR/../emodul/parse.sh 1009 sensor`
+	EM_TEMP3=`$DIR/../../emodul/parse.sh 1009 sensor`
 	echo "EM_TEMP3=$EM_TEMP3"
 	URL_PARAMS="$URL_PARAMS&field$FIELD_NUM=$EM_TEMP3"
 	FIELD_NUM=$(($FIELD_NUM+1))
 
-	EM_FAN=`$DIR/../emodul/parse.sh 1011 fan`
+	EM_FAN=`$DIR/../../emodul/parse.sh 1011 fan`
 	echo "EM_FAN=$EM_FAN"
 	URL_PARAMS="$URL_PARAMS&field$FIELD_NUM=$EM_FAN"
 	FIELD_NUM=$(($FIELD_NUM+1))
--- upload/pi/channel2.sh
+++ upload/pi/channel2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DIR=$(dirname $0)
-source $DIR/config.sh
+source $DIR/../config.sh
 
 API_KEY=$API_KEY2
 
@@ -13,49 +13,49 @@ echo $(date)
 ###
 ### w1
 ###
-#TEMP1=$($DIR/../temperature/w1/get.sh "28-0416812d86ff")
+#TEMP1=$($DIR/../../temperature/w1/get.sh "28-0416812d86ff")
 #echo "TEMP1=$TEMP1"
 #if [ ! -z $TEMP1 ]; then
 #   URL_PARAMS="$URL_PARAMS&field1=$TEMP1"
 #fi
 
-#TEMP2=$($DIR/../temperature/w1/get.sh "28-0416816c14ff")
+#TEMP2=$($DIR/../../temperature/w1/get.sh "28-0416816c14ff")
 #echo "TEMP2=$TEMP2"
 #if [ ! -z $TEMP2 ]; then
 #   URL_PARAMS="$URL_PARAMS&field2=$TEMP2"
 #fi
 
-TEMP3=$($DIR/../temperature/w1/get.sh "28-03168123a0ff")
+TEMP3=$($DIR/../../temperature/w1/get.sh "28-03168123a0ff")
 echo "TEMP3=$TEMP3"
 if [ ! -z $TEMP3 ]; then
    URL_PARAMS="$URL_PARAMS&field3=$TEMP3"
 fi
 
-TEMP4=$($DIR/../temperature/w1/get.sh "28-0316811ffaff")
+TEMP4=$($DIR/../../temperature/w1/get.sh "28-0316811ffaff")
 echo "TEMP4=$TEMP4"
 if [ ! -z $TEMP4 ]; then
    URL_PARAMS="$URL_PARAMS&field4=$TEMP4"
 fi
 
-TEMP5=$($DIR/../temperature/w1/get.sh "28-0416810106ff")
+TEMP5=$($DIR/../../temperature/w1/get.sh "28-0416810106ff")
 echo "TEMP5=$TEMP5"
 if [ ! -z $TEMP5 ]; then
    URL_PARAMS="$URL_PARAMS&field5=$TEMP5"
 fi
 
-TEMP6=$($DIR/../temperature/w1/get.sh "28-0416812edaff")
+TEMP6=$($DIR/../../temperature/w1/get.sh "28-0416812edaff")
 echo "TEMP6=$TEMP5"
 if [ ! -z $TEMP6 ]; then
   URL_PARAMS="$URL_PARAMS&field6=$TEMP6"
 fi
 
-TEMP7=$($DIR/../temperature/w1/get.sh "28-0516848757ff")
+TEMP7=$($DIR/../../temperature/w1/get.sh "28-0516848757ff")
 echo "TEMP7=$TEMP7"
 if [ ! -z $TEMP7 ]; then
   URL_PARAMS="$URL_PARAMS&field7=$TEMP7"
 fi
 
-TEMP8=$($DIR/../temperature/ea2/get_hum.sh 42)
+TEMP8=$($DIR/../../temperature/ea2/get_hum.sh 42)
 echo "TEMP8=$TEMP8"
 if [ ! -z $TEMP8 ]; then
   URL_PARAMS="$URL_PARAMS&field8=$TEMP8"
