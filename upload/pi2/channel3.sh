7bf9532a2250624c9749abef66a73802313b7cb3
Andriy Kulchytskyy <kulchytskyy@gmail.com>
2020-06-22 15:25:51 +0300

    fixed paths

--- upload/pi2/channel3.sh
+++ upload/pi2/channel3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DIR=$(dirname $0)
-source $DIR/config.sh
+source $DIR/../config.sh
 
 API_KEY=$API_KEY3
 
@@ -12,19 +12,19 @@ echo $(date)
 ###
 ### w1
 ###
-TEMP1=$($DIR/../temperature/w1/get.sh "28-0316819430ff")
+TEMP1=$($DIR/../../temperature/w1/get.sh "28-0316819430ff")
 echo "TEMP1=$TEMP1"
 if [ ! -z $TEMP1 ]; then
    URL_PARAMS="$URL_PARAMS&field1=$TEMP1"
 fi
 
-TEMP2=$($DIR/../temperature/w1/get.sh "28-0000052cf9c6")
+TEMP2=$($DIR/../../temperature/w1/get.sh "28-0000052cf9c6")
 echo "TEMP2=$TEMP2"
 if [ ! -z $TEMP2 ]; then
    URL_PARAMS="$URL_PARAMS&field2=$TEMP2"
 fi
 
-TEMP3=$($DIR/../temperature/w1/get.sh "28-0416816c14ff")
+TEMP3=$($DIR/../../temperature/w1/get.sh "28-0416816c14ff")
 echo "TEMP3=$TEMP3"
 if [ ! -z $TEMP3 ]; then
    URL_PARAMS="$URL_PARAMS&field3=$TEMP3"
