e836a9a87ec245486e5567278df164410c82983f
Andriy Kulchytskyy <kulchytskyy@gmail.com>
2020-12-03 10:06:17 +0200

    Separated heating and shutter up/down scripts

--- relay/heating/up.sh
+++ relay/heating/up.sh
@@ -1,1 +1,23 @@
-../up.sh
+#!/bin/bash
+
+DIR=$(dirname "$0")
+LOGFILE=/var/log/ha/dhw.log
+
+source $DIR/../config.sh
+if [ -f $DIR/config.sh ]; then
+	source $DIR/config.sh
+fi
+source $DIR/pins.sh
+
+echo "`date -u` Switching to water heating" | tee $LOGFILE
+
+if [[ `cat /var/ha/boiler/dhw` == "WATER" ]]; then
+	echo "Already heating water" | tee $LOGFILE
+	exit
+fi
+
+echo "WATER" > /var/ha/boiler/dhw
+
+bash $DIR/../run.sh $UP_PIN $DOWN_PIN
+
+ 
