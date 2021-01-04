ce9d7b5a08963338c56f1bfc906ddaa49860bd89
Andriy Kulchytskyy <kulchytskyy@gmail.com>
2021-01-04 10:23:49 +0200

    Switched to rtl from repository

--- temperature/ea2/daemon.sh
+++ temperature/ea2/daemon.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 #rtl_433 -R 31 -q -F csv 
-~/rtl_433/build/src/rtl_433 -R 31 -R 155 -q -F csv 
+#~/rtl_433/build/src/rtl_433 -R 31 -R 155 -q -F csv 
+rtl_433 -R 31 -R 165 -q -F csv 
 
 
 #>> /var/local/ea2temp.csv 2>/var/local/ea2.err &
