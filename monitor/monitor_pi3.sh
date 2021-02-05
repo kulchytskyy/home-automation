8e2e1d2b06d7a2822bbc35eb5de1fd3fb15d597b
Andriy Kulchytskyy <kulchytskyy@gmail.com>
2021-02-05 13:36:42 +0200

    pi3 monitoring

---
+++ monitor/monitor_pi3.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+DIR=$(dirname $0)
+
+source $DIR/func.sh
+
+echo
+echo "`date`"
+
+check_in_range attic 10 30
+check_in_range kidroom 17 26
+check_in_range bathroom2 16 26
+check_in_range bedroom 17 26
+check_in_range kidroom2 16 26
+check_in_range floor1 15 26
+check_in_range floor2 15 26
+check_in_range hum_living 30 80
+check_in_range hum_outdoor 10 100
+check_in_range hum_floor1 30 80
+check_in_range hum_floor2 30 80
+
+check_in_range boiler_ch_temp 15 70
+check_in_range boiler_dhw 15 70
+
+check_in_range livingroom 18 26
+check_in_range bathroom 16 26
+check_in_range outdoor -15 30
+
