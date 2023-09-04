cb3c6565c47552ac508b1ca6a46bae20db9a5f1d
Andriy Kulchytskyy <kulchytskyy@gmail.com>
2023-09-04 14:33:45 +0300

    moved apikey out of code

--- relay/water_boiler/is_sunny.sh
+++ relay/water_boiler/is_sunny.sh
@@ -4,13 +4,10 @@ CLOUDCOVER_MAX=50
 
 DIR=$(dirname "$0")
 source $DIR/config.sh
-
-#curl "https://api.open-meteo.com/v1/forecast?latitude=49.759&longitude=24.1621&hourly=direct_radiation&forecast_days=0&current_weather=true"
-#http://api.openweathermap.org/data/2.5/weather?q=Lviv&APPID=d551dad1df30d43e3ea57cd675a4fd49
+source ~/.ha_creds
 
 CITY="Lviv"
-APIKEY="d5027eef80e2387e3e8f3675e25daa74"
-URL="http://api.weatherstack.com/current?access_key=$APIKEY&query=$CITY"
+URL="http://api.weatherstack.com/current?access_key=$WEATHERSTACK_APIKEY&query=$CITY"
 #echo $URL
 
 DATA=`wget -q -O- $URL`
